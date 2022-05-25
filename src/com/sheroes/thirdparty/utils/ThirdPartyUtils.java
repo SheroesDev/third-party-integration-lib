93cead9b48b247064d929049cd0d4b3d7ae6db4a
yosef girma <yosef.gkel@gmail.com>
2022-05-25 09:12:40 +0300

    modifiy third party build

--- src/com/sheroes/thirdparty/utils/ThirdPartyUtils.java
+++ src/com/sheroes/thirdparty/utils/ThirdPartyUtils.java
@@ -3,18 +3,13 @@ package com.sheroes.thirdparty.utils;
 import com.nimbusds.jose.EncryptionMethod;
 import com.nimbusds.jose.JWEAlgorithm;
 import com.nimbusds.jose.JWEHeader;
-import com.nimbusds.jose.crypto.RSADecrypter;
 import com.nimbusds.jose.crypto.RSAEncrypter;
 import com.nimbusds.jwt.EncryptedJWT;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.sheroes.thirdparty.model.ClaimResponse;
 import com.sheroes.thirdparty.model.ThirdPartyClaim;
-import com.sheroes.thirdparty.model.Token;
 import com.sheroes.thirdparty.model.ValidationResponse;
-
-import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
-import java.util.Map;
 
 public class ThirdPartyUtils {
 
@@ -34,7 +29,6 @@ public class ThirdPartyUtils {
 				JWEHeader header = new JWEHeader(JWEAlgorithm.RSA_OAEP_256, EncryptionMethod.A256CBC_HS512);
 				EncryptedJWT jwt = new EncryptedJWT(header, claimsSet.build());
 				RSAEncrypter encrypter = new RSAEncrypter(publicRsaKey);
-				// Doing the actual encryption
 				jwt.encrypt(encrypter);
 				encryptedValue = jwt.serialize();
 				claimResponse.setSuccess(true);
@@ -52,28 +46,6 @@ public class ThirdPartyUtils {
 
 		}
 		return claimResponse;
-	}
-
-	public static ThirdPartyClaim decrypt(Token tokenParse) {
-
-		ThirdPartyClaim claims = new ThirdPartyClaim();
-
-		if (tokenParse.getToken() == null) {
-			return null;
-		}
-		try {
-
-			EncryptedJWT jwt = EncryptedJWT.parse(tokenParse.getToken());
-			RSAPrivateKey privateRsaKey = (RSAPrivateKey) PrivateKeyReader.get();
-			RSADecrypter decrypter = new RSADecrypter(privateRsaKey);
-			jwt.decrypt(decrypter);
-			Map<String, Object> claimMap = jwt.getJWTClaimsSet().getClaims();
-			claims = getThirdPartyClaim(claimMap);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return claims;
 	}
 
 	private static ValidationResponse validateClaim(ThirdPartyClaim claim) {
@@ -115,16 +87,4 @@ public class ThirdPartyUtils {
 		return claimsSet;
 	}
 
-	private static ThirdPartyClaim getThirdPartyClaim(Map<String, Object> claimMap) {
-		ThirdPartyClaim claims = new ThirdPartyClaim();
-		claims.setApikey(String.valueOf(claimMap.get("apiKey")));
-		claims.setApiSecret(String.valueOf(claimMap.get("apiSecret")));
-		claims.setFirstName(String.valueOf(claimMap.get("firstName")));
-		claims.setLastName(String.valueOf(claimMap.get("lastName")));
-		claims.setMobile(String.valueOf(claimMap.get("mobile")));
-		claims.setEmail(String.valueOf(claimMap.get("email")));
-		claims.setLang(String.valueOf(claimMap.get("lang")));
-		claims.setLocation(String.valueOf(claimMap.get("loc")));
-		return claims;
-	}
 }
